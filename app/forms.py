a441ad26face333b43f8264f230de806f0d40cba
jobaer-bs23 <jobaer.ahamed@brainstation-23.com>
2024-04-04 17:52:05 +0600

    add reset password

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm , AuthenticationForm,UsernameField,PasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm , AuthenticationForm,UsernameField,PasswordChangeForm,PasswordResetForm, SetPasswordForm
 from django.contrib.auth.models import User
 from django.utils.translation import gettext,gettext_lazy as _
 from django.contrib.auth import password_validation
@@ -24,3 +24,19 @@ class MyPasswordChangeForm(PasswordChangeForm):
     old_password = forms.CharField(label=_('Old Password'), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'current_password', 'autofucus':True, 'class':'form-control'}))
     new_password1 = forms.CharField(label=_('New Password'), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new_password', 'autofucus':True, 'class':'form-control'}), help_text=password_validation.password_validators_help_text_html())
     new_password2 = forms.CharField(label=_('Confirm Your Password'), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new_password', 'autofucus':True, 'class':'form-control'}))
+    
+class MyPasswordResetForm(PasswordResetForm):
+    email = forms.EmailField(label=_("Email"),max_length=254,widget=forms.EmailInput(attrs={"autocomplete": "email",'class':'form-control'}))
+
+class MySetPasswordForm(SetPasswordForm):
+    new_password1 = forms.CharField(
+        label=_("New password"),
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password",'class':'form-control'}),
+        strip=False,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    new_password2 = forms.CharField(
+        label=_("New password confirmation"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password",'class':'form-control'}),
+    )
